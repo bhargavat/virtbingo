b00c0af8712ce69c55b7a26830e01a227685879b
bhargavat <komalis@Bhargavas-MBP-5.T-mobile.com>
2016-07-04 03:02:36 -0700

    Refactoring

--- js/app.js
+++ js/app.js
@@ -1,65 +1,50 @@
 var used = []; //array of values already used
 var marked = "#f75757";
+var clicked = false; //if board was clicked
 
-function submitted(){ //after 'Generate' button is pressed
+$(document).on("click", "#player", function (e) { //after 'Generate' button is pressed
 	var row = 1;
 	var col = 1;
 	var id = "r1-c1";
 	var rand = 0;
-	for (var i = 0; i < 5; i++) {
-		for(var j = 0; j < 5; j++){
-			var c = parseInt(id.substring(4,5));
-			rand = randomNumber(c);
-			while(isDuplicate(rand)){
-				rand = randomNumber(c);
-			} 
-			document.getElementById(id).innerHTML = rand.toString();
-			used.push(rand);
-			if(id=="r3-c3"){
-			document.getElementById(id).innerHTML = "FREE";
-			document.getElementById(id).style.backgroundColor = marked;	
+
+var table = document.getElementById("vboard");
+for (var i = 1, row; row = table.rows[i]; i++) {
+		for (var j = 0, col; col = row.cells[j]; j++) {
+						rand = randomNumber(j);
+						var current = table.rows[i].cells[j];
+						console.log(rand);
+						while(isDuplicate(rand)) rand = randomNumber(j);
+						$(current).html(rand.toString());
+						used.push(rand);
+						if(i==3 && j==2){
+								$(current).html("FREE");
+						}
+						else if($(current).css("background-color") != "white")
+							$(current).css("background-color","white");
 			}
-			id = nextId(id);
-		}
 	}
 	used = [];
-}
+});
 
 // Alternates color for player board
 $(document).on("click", "#vboard td", function (e) {
 	if(this.id == "" || this.id == null) return; //ignores for first row (BINGO)
 
     var id = this.id;
-    if(((document.getElementById(id).style.backgroundColor == "white")
-		|| (document.getElementById(id).style.backgroundColor == ""))
-		&& (document.getElementById(id).innerHTML) != '?'){
-		document.getElementById(id).style.backgroundColor = marked;	
+		if(id=="r3-c3") return; // leave FREE space untouched
+    if(((this.style.backgroundColor == "white")
+		|| (this.style.backgroundColor == ""))){
+		this.style.backgroundColor = marked;
 	}else{
-		document.getElementById(id).style.backgroundColor = "white";
+		this.style.backgroundColor = "white";
 	}
+
 });
 
 function removeElement(e){
 	var elem = document.getElementById(e);
     elem.parentNode.removeChild(elem);
-}
-
-function resetColor(id){
-	if(document.getElementById(id).style.backgroundColor != "white"){
-		document.getElementById(id).style.backgroundColor == "white";
-	}
-}
-
-function nextId(id){ //r1-c2 -> r1-c3
-	var r = parseInt(id.substring(1,2));
-	var c = parseInt(id.substring(4,5));
-	if(c==5){ 
-	r++;
-	c=1;
-	}else{
-	c++;
-	}
-	return "r"+r+"-c"+c;
 }
 
 function isDuplicate(num){
@@ -71,9 +56,9 @@ return false;
 }
 
 function randomNumber(col){
-if(col == 1) return Math.floor((Math.random()*15)+1); //1-15 (B)
-else if(col == 2) return Math.floor((Math.random()*15)+16); //16-30 (I)
-else if(col == 3) return Math.floor((Math.random()*15)+31); //31-45 (N)
-else if(col == 4) return Math.floor((Math.random()*15)+46); //46-60 (G)
-else if(col == 5) return Math.floor((Math.random()*15)+61); //61-75 (O)
+if(col == 0) return Math.floor((Math.random()*15)+1); //1-15 (B)
+else if(col == 1) return Math.floor((Math.random()*15)+16); //16-30 (I)
+else if(col == 2) return Math.floor((Math.random()*15)+31); //31-45 (N)
+else if(col == 3) return Math.floor((Math.random()*15)+46); //46-60 (G)
+else if(col == 4) return Math.floor((Math.random()*15)+61); //61-75 (O)
 }
--- js/display.js
+++ js/display.js
@@ -1,38 +1,27 @@
 //controls what's being displayed
 
-$(document).on("click", "#menu1", function (e) {
-    $("#menu1").fadeOut(500);
-    $("#card").fadeOut(500);
-    $("#select").fadeIn(1500);
-});
-
-$(document).on("click", "#menu2", function (e) {
-    $("#menu2").fadeOut(500);
-    $("#numgen").fadeOut(500);
-    $("#select").fadeIn(1500);
-    resetBoard('menu');
-});
-
 $(document).on("click", "#player", function (e) {
-    $("#select").fadeOut(200);
+    document.getElementById("select").style.display = "none";
     $("#card").fadeIn(1000);
-    submitted();
     $("#menu1").fadeIn(1000);
+    submitted();
 });
 
 $(document).on("click", "#host", function (e) {
-  // var btns = document.getElementById("select");
-  // btns.parentNode.removeChild(btns);
-  $("#select").fadeOut(100);
-  $("#numgen").fadeIn(1000);
+  document.getElementById("select").style.display = "none";
   $("#numgen").fadeIn(1000);
   $("#menu2").fadeIn(1000);
   document.body.style.background = "#bbbfc2";
 });
-// function hostMode(){
-//     var btns = document.getElementById("select");
-//     btns.parentNode.removeChild(btns);
-//
-//     document.getElementById("numgen").style.display = "block";
-//     document.body.style.background = "#bbbfc2";
-// }
+
+$(document).on("click", "#menu1", function (e) {
+    document.getElementById("card").style.display = "none";
+    $("#select").fadeIn(1000);
+});
+
+$(document).on("click", "#menu2", function (e) {
+    document.getElementById("numgen").style.display = "none";
+    $("#select").fadeIn(1000);
+    resetBoard('menu');
+    document.body.style.background = "linear-gradient(to right, #fbc0c0, #ace7f9)";
+});
